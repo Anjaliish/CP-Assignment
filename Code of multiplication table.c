785e16006064d249544c3b3c29f427746406e382
Anjali Sharma <73048806+Anjaliish@users.noreply.github.com>
2021-07-17 23:42:43 +0530

    Create Code of multiplication table.c

---
+++ Code of multiplication table.c
@@ -0,0 +1,15 @@
+//Multiplication table//
+#include <stdio.h>
+
+int main()
+{
+    int i,n;
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    
+    for(i=1;i<=10;i++){
+        printf("%d*%d=%d\n",n,i,n*i);
+    }
+
+    return 0;
+}
